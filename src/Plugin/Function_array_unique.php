78fcee195ab9410da27cc04c2422e045a8c44718
Remco van der Velde <remco@priya.software>
2020-09-19 19:18:25 +0200

    Plugin: -more array functions

---
+++ src/Plugin/Function_array_unique.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-09-19
+ * @copyright       Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *     -            all
+ */
+use R3m\Io\Module\Parse;
+use R3m\Io\Module\Data;
+
+function function_array_unique(Parse $parse, Data $data, $array=[], $sort=SORT_STRING){
+    if(is_string($sort)){
+        $sort = constant($sort);
+    }
+    $result = array_unique($array, $sort);
+    return $result;
+}
---
+++ src/Plugin/Function_array_unshift.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-09-19
+ * @copyright       Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *     -            all
+ */
+use R3m\Io\Module\Parse;
+use R3m\Io\Module\Data;
+
+function function_array_unshift(Parse $parse, Data $data, $selector='', $value=''){
+    if(substr($selector, 0, 1) == '$'){
+        $selector = substr($selector, 1);
+    }
+    $array = $data->data($selector);
+    $result = array_unshift($array, $value);
+    $data->data($selector, $array);
+    return $result;
+}
