fa22376f6faf49a232da1cecd8e687de73bb7ea7
Remco van der Velde <remco.pc@outlook.com>
2021-08-01 23:18:03 +0200

    export translation & language for javascript import

--- src/Plugin/Function_export_translation.php
+++ src/Plugin/Function_export_translation.php
@@ -6,18 +6,20 @@
 use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
 
-function function_import_translation(Parse $parse, Data $data){
+function function_export_translation(Parse $parse, Data $data, $type='object'){
     $object = $parse->object();
     $url = $object->config('controller.dir.data') . $object->config('dictionary.translation') . $object->config('ds');
     $dir = new Dir();
     $read = $dir->read($url);
+    $export = new Data();
     if($read){
         foreach($read as $nr => $file){
             $file->basename = File::basename($file->name, $object->config('extension.json'));
-            $translation = $object->data_read($file->url, sha1($file->url), true);
-            if($translation){
-                $object->data('translation.' . strtolower($file->basename), $translation->data());
-            }
+            $export->data(
+                'translation.' . strtolower($file->basename),
+                $object->data('translation.' . strtolower($file->basename))
+            );
         }
     }
+    return Core::object($export->data(), $type);
 }
--- src/Plugin/Function_language.php
+++ src/Plugin/Function_language.php
@@ -11,25 +11,15 @@
 use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
 
-function function___(Parse $parse, Data $data, $attribute=null){
+function function_language(Parse $parse, Data $data, $attribute=null){
     $object = $parse->object();
-    $language = $object->session('language');
-    if($language === null){
-        $language = $object->session('language', $object->config('framework.default.language'));
-    }
-    $test = $object->data('translation');
-    if(empty($test)){
-        return '{import.translation()} missing or corrupted translation file...' . PHP_EOL;
-    }
-    $translation = $object->data('translation.' . $language);
-    if(property_exists($translation, $attribute)){
-        return $translation->{$attribute};
-    } else {
-        $translation = $object->data('translation.' . $object->config('framework.default.language'));
-        if(property_exists($translation, $attribute)){
-            return $translation->{$attribute};
-        } else {
-            return $attribute;
+    if($attribute === null){
+        $language = $object->session('language');
+        if($language === null){
+            $language = $object->session('language', $object->config('framework.default.language'));
         }
+    } else {
+        $language = $object->session('language', $attribute);
     }
+    return $language;
 }
