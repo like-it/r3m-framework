88af7987fd3d6d76a1d803da1e5cd74e7dceedc4
Remco van der Velde <remco.pc@outlook.com>
2021-08-14 14:44:37 +0200

    load routes

---
+++ src/Cli/Info/Plugin/Function_route.load.php
@@ -0,0 +1,35 @@
+<?php
+
+use R3m\Io\Module\Parse;
+use R3m\Io\Module\Data;
+use R3m\Io\Module\Config;
+use R3m\Io\Module\Dir;
+use R3m\Io\Module\File;
+
+function function_binary_create(Parse $parse, Data $data, $name=null){    
+    if(empty($name)){
+        $name = \R3m\Io\Cli\Bin\Controller\Bin::DEFAULT_NAME;
+    }
+    $object = $parse->object();
+    $execute = $object->config(Config::DATA_PROJECT_DIR_BINARY) . \R3m\Io\Cli\Bin\Controller\Bin::EXE;
+    Dir::create($object->config(Config::DATA_PROJECT_DIR_BINARY), Dir::CHMOD);
+    $dir = Dir::name(\R3m\Io\Cli\Bin\Controller\Bin::DIR) .
+        $object->config(
+            Config::DICTIONARY .
+            '.' .
+            Config::DATA
+        ) .
+        $object->config('ds');
+    $source = $dir . \R3m\Io\Cli\Bin\Controller\Bin::EXE;      
+    if(File::exist($execute)){
+        File::delete($execute);
+    }    
+    File::copy($source, $execute);
+    $url = \R3m\Io\Cli\Bin\Controller\Bin::TARGET . $name;
+    $content = [];
+    $content[] = '#!/bin/sh';
+    $content[] = 'php ' . $execute . ' "$@"';
+    $content = implode(PHP_EOL, $content);
+    File::write($url, $content);
+    shell_exec('chmod +x ' . $url);
+}
