1036a3e590da6642f3e674478e4e40ccbbb7ce1d
Remco van der Velde <remco.pc@outlook.com>
2021-09-03 13:13:15 +0200

    bugfix ln
    bugfix ln

--- src/Cli/Ln/Controller/Ln.php
+++ src/Cli/Ln/Controller/Ln.php
@@ -18,7 +18,7 @@ use R3m\Io\Exception\UrlNotExistException;
 
 class Ln extends View {
     const DIR = __DIR__;
-    const NAME = 'ln';
+    const NAME = 'Ln';
     const INFO = '{binary()} ln                             | ln  creates a symlink if not exist';
     
     public static function run($object){
