e1ca8a040ed3b60a32ef29d79e0e1a197bd8b803
Remco van der Velde <remco.pc@outlook.com>
2021-09-16 22:23:33 +0200

    Update Function_admin_taskrunner.php

--- src/Cli/Admin/Plugin/Function_admin_taskrunner.php
+++ src/Cli/Admin/Plugin/Function_admin_taskrunner.php
@@ -9,7 +9,6 @@ use R3m\Io\Module\Parse;
 
 function function_admin_taskrunner(Parse $parse, Data $data){
     $object = $parse->object();
-
     $dir = new Dir();
     $read = $dir->read($object->config('project.dir.data') . 'Input' . $object->config('ds'), true);
     foreach($read as $nr => $file){
@@ -21,24 +20,10 @@ function function_admin_taskrunner(Parse $parse, Data $data){
             echo implode(PHP_EOL, $output);
             $content = ob_get_contents();
             ob_end_clean();
-            dd($content);
+            $basename = File::basename($file->url);
+            $dir = $object->config('project.dir.data') . 'Output' . $object->config('ds');
+            Dir::create($dir);
+            File::write($dir . $basename, $content);
         }
     }
-    dd($read);
-
-
-    /*
-    $task  = App::parameter($object, 'task', 1);
-    $euid = posix_geteuid();
-    $dir = $object->config('project.dir.data') .'Input' . $object->config('ds');
-    $dir_part = $euid . $object->config('ds');
-    Dir::create($dir . $dir_part);
-    $uuid = Core::uuid();
-    $url_part = $dir_part . $uuid . '.task';
-    $url = $dir . $url_part;
-    File::write($url, $task);
-    return $url_part . PHP_EOL;
-    */
-
-
 }
