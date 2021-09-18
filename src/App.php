d4f3e6a1b042b436a22928b7109cc201ce58b05b
Remco van der Velde <remco.pc@outlook.com>
2021-09-18 23:48:15 +0200

    Update App.php

--- src/App.php
+++ src/App.php
@@ -103,6 +103,7 @@ class App extends Data {
                                 $route->method
                             )
                         ){
+                            d($object->request());
                             d($route);
                         }
                         App::contentType($object);
