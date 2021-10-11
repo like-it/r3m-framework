143a899cbf44b85f0d44e258de39a873418cea64
Remco van der Velde <remco.pc@outlook.com>
2021-10-11 15:57:51 +0200

    Update Function_route_add.php

--- src/Cli/Configure/Plugin/Function_route_add.php
+++ src/Cli/Configure/Plugin/Function_route_add.php
@@ -24,6 +24,7 @@ function function_route_add(Parse $parse, Data $data, $add=''){
         }
         if($has_route){
             $read = $object->data_read($has_route->resource);
+            d($has_route->resource);
             d($add);
             dd($read);
         } else {
