65071256c6e1cf881b6d1076bb66a13574413db9
Remco van der Velde <remco.pc@outlook.com>
2022-01-04 22:39:03 +0100

    Create Validate_in_array.php

---
+++ src/Validate/Validate_in_array.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-09-18
+ * @copyright       Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *     -            all
+ */
+use R3m\Io\Module\Parse\Token;
+
+function validate_in_array(R3m\Io\App $object, $field='', $array=''){
+    $in = $object->request('node.' . $field);
+    if(empty($in)){
+        $in = $object->request($field);
+    }
+    if(empty($in)){
+        return false;
+    }
+    d($in);
+    dd($array);
+    /*
+    foreach($argument[1]['method']['attribute'][0] as $nr => $record){
+        if(empty($left)){
+            $left = $record;
+        }
+        elseif(empty($equation)){
+            $equation = $record['value'];            
+        }
+        elseif(empty($right)){
+            $right = $record['execute'];
+            break;
+        }
+    }
+    */
+    return in_array($in, $array);
+}
