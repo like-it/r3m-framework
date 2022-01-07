c5b66a3a4047e7f206193778810f6f7883822199
Remco van der Velde <remco.pc@outlook.com>
2022-01-07 19:23:12 +0100

    Update Validate_string_contains.php

--- src/Validate/Validate_string_contains.php
+++ src/Validate/Validate_string_contains.php
@@ -26,8 +26,10 @@ function validate_string_contains(R3m\Io\App $object, $field='', $argument=''){
                 $string,
             $matches
             );
-            d($string);
-            dd($matches);
+            if(property_exists($argument, 'debug') && $argument->debug === true){
+                d($string);
+                dd($matches);
+            }
             if(array_key_exists(0, $matches)){
                 if($string !== $matches[0]){
                     return false;
