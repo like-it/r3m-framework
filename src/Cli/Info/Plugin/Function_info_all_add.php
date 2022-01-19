30d80e839361f8c6f97d160b46500fb48ab0b6be
Remco van der Velde <remco.pc@outlook.com>
2022-01-20 00:03:24 +0100

    Update Function_info_all_add.php

--- src/Cli/Info/Plugin/Function_info_all_add.php
+++ src/Cli/Info/Plugin/Function_info_all_add.php
@@ -28,6 +28,7 @@ function function_info_all_add(Parse $parse, Data $data, $list){
                 Cli::tput('init');
                 echo Cli::tput('background', CLI::COLOR_RED);
                 echo PHP_EOL;
+                echo PHP_EOL;
                 echo $exception->getMessage() . PHP_EOL;
                 echo PHP_EOL;
                 echo Cli::tput('reset');
