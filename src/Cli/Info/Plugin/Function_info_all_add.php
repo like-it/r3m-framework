edb31ecd4c60d0b0dd240c5e119974ef0ca7ac23
Remco van der Velde <remco.pc@outlook.com>
2022-01-19 23:23:04 +0100

    Update Function_info_all_add.php

--- src/Cli/Info/Plugin/Function_info_all_add.php
+++ src/Cli/Info/Plugin/Function_info_all_add.php
@@ -24,11 +24,11 @@ function function_info_all_add(Parse $parse, Data $data, $list){
                 $record->info = $info;
                 $result[] = $record;
             } catch (Exception $exception){
-                Cli::tput('init');
+                Core::execute('tput init');
                 for($i=0; $i < 16; $i++){
-                    Cli::tput('background', $i);
+                    Core::execute('tput setab', $i);
                     echo 'test' . PHP_EOL;
-                    Cli::tput('reset');
+                    Core::execute('tput sgr0');
                 }
                 continue;
             }
