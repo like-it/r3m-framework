dcb2a821d298c5e0793b091d91e064d65c029502
Remco van der Velde <remco.pc@outlook.com>
2022-03-13 21:47:38 +0100

    clean up

--- src/Plugin/Function_logger_critical.php
+++ src/Plugin/Function_logger_critical.php
@@ -5,8 +5,6 @@
  */
 use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
-use R3m\Io\Module\File;
-use R3m\Io\Module\Core;
 
 function function_logger_critical(Parse $parse, Data $data, $message=null, $context=[]){
     $object = $parse->object();
