f84024e9391d5f20198a63bb91f348865fa257da
Remco van der Velde <remco.pc@outlook.com>
2022-04-06 23:18:43 +0200

    Zip archive

---
+++ src/Cli/Zip/Plugin/Function_Zip.php
@@ -0,0 +1,19 @@
+<?php
+
+use R3m\Io\Module\Parse;
+use R3m\Io\Module\Data;
+use R3m\Io\Module\Core;
+use R3m\Io\Module\File;
+use R3m\Io\App;
+
+function function_zip(Parse $parse, Data $data){
+    $object = $parse->object();
+
+    $source = App::parameter($object, 'zip', 1);
+    $target = App::parameter($object, 'zip', 2);
+
+    if(File::exist($target)){
+        return;
+    }
+    Core::execute('ln -s ' . $source . ' ' . $target);
+}
