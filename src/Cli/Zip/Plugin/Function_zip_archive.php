52ae4f0b9145a426f911f121bf4ad2cb62b1491f
Remco van der Velde <remco.pc@outlook.com>
2022-04-07 12:43:10 +0200

    zip archive

--- src/Cli/Zip/Plugin/Function_zip_archive.php
+++ src/Cli/Zip/Plugin/Function_zip_archive.php
@@ -6,17 +6,13 @@ use R3m\Io\Module\Core;
 use R3m\Io\Module\File;
 use R3m\Io\App;
 
-function function_zip(Parse $parse, Data $data){
+function function_zip_archive(Parse $parse, Data $data){
     $object = $parse->object();
     $object->logger()->error('test2: zip');
-    $archive = App::parameter($object, 'zip', 1);
-    dd($object->request());
-    if($archive === 'archive'){
-        $source = App::parameter($object, 'archive', 1);
-        $target = App::parameter($object, 'archive', 2);
-        d($source);
-        dd($target);
-    }
+    $source = App::parameter($object, 'archive', 1);
+    $target = App::parameter($object, 'archive', 2);
+    d($source);
+    dd($target);
 
 
 
