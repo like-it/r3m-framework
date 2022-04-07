1bd7787ef84ac76c15f1cddc642ee5d5c33d89f2
Remco van der Velde <remco.pc@outlook.com>
2022-04-07 16:11:33 +0200

    Update Build.php

--- src/Module/Parse/Build.php
+++ src/Module/Parse/Build.php
@@ -19,11 +19,10 @@ use R3m\Io\Module\Data;
 use R3m\Io\Module\File;
 use R3m\Io\Module\Dir;
 use R3m\Io\Module\Autoload;
-use R3m\Io\Module\Parse;
 
 use Exception;
 use R3m\Io\Exception\PluginNotFoundException;
-
+use R3m\Io\Exception\PluginNotAllowedException;
 
 class Build {
     const NAME = 'Build';
