b748ed601d015d05d556d3d0103a8034366bca00
Remco van der Velde <remco.pc@outlook.com>
2022-05-05 16:44:23 +0200

    Update Function_trait.php

--- src/Plugin/Function_trait.php
+++ src/Plugin/Function_trait.php
@@ -11,9 +11,19 @@
 use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
 
-function function_trait(Parse $parse, Data $data, $name='', $value=null, $test=null){
+function function_trait(Parse $parse, Data $data, $name='', $value=null){
+    $object = $parse->object();
+
+    dd($object->data());
+    $explode = explode(':', $name);
+    if(array_key_exists(1, $explode)){
+        $namespace = $explode[0];
+        $name = $explode[1];
+    } else {
+        $namespace = '';
+        $name = $explode[0];
+    }
     d($name);
-    d($test);
     dd($value);
 
 }
