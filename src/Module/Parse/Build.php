03a52815c16df5c8602c7be0041983014023bf4a
Remco van der Velde <remco.pc@outlook.com>
2022-05-08 03:30:40 +0200

    Update Build.php

--- src/Module/Parse/Build.php
+++ src/Module/Parse/Build.php
@@ -160,7 +160,7 @@ class Build {
     private function createTrait($document=[]){
         $storage = $this->storage();
         $trait = [];
-        $use[];
+        $use= [];
         $list = $storage->data('trait');
         if(
             is_array($list) ||
