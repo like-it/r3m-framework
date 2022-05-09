25fc0d90a224c47e1df58c08fc5e2b3271074901
Remco van der Velde <remco.pc@outlook.com>
2022-05-09 14:37:32 +0200

    Update Function_import.php

--- src/Plugin/Function_import.php
+++ src/Plugin/Function_import.php
@@ -149,6 +149,7 @@ function function_import(Parse $parse, Data $data, $url=null, $controller=null, 
                 $record['namespace'] = null;
                 $record['trait'] = $explode[0];
             }
+            dd($parse->storage());
             $list = $parse->storage()->get('import.trait');
             if(empty($list)){
                 $list = [];
