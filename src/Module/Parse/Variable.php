049295e833a0e63af5ba00a0a19a37fac54b45db
Remco van der Velde <remco.pc@outlook.com>
2022-05-10 21:57:19 +0200

    Update Variable.php

--- src/Module/Parse/Variable.php
+++ src/Module/Parse/Variable.php
@@ -201,7 +201,10 @@ class Variable {
         }
         $operator = $token;
         while(Operator::has($operator)){            
-            $statement = Operator::get($operator);            
+            $statement = Operator::get($operator);
+            if($statement === false){
+                dd($operator);
+            }
             $operator = Operator::remove($operator, $statement);
             $statement = Operator::create($build, $storage, $statement);
             if(empty($statement)){
