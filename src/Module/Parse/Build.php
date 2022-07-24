d47e2843d0ac190c86583753f8808c44eb4c2806
Remco van der Velde <remco.pc@outlook.com>
2022-07-24 23:43:52 +0200

    Update Build.php

--- src/Module/Parse/Build.php
+++ src/Module/Parse/Build.php
@@ -408,7 +408,8 @@ class Build {
         $write = str_replace($this->storage()->data('placeholder.generation.time'), round($this->storage()->data('time.duration') * 1000, 2). ' msec', $write);
         $dir = Dir::name($url);
         Dir::create($dir);
-        $write =  File::write($url, $write);    //maybe use a different method (to check where the bug is coming from)
+        $put = file_put_contents($url, $write, LOCK_EX); //test if bug doesn't get there...
+//        $write =  File::write($url, $write);    //maybe use a different method (to check where the bug is coming from)
         $command = 'php -l ' . escapeshellcmd($url);
         Core::execute($command, $output, $error);
         if($output){
