8cc9e5e7ef21f4bf36ff90e80c69a5a0c904fc58
Remco van der Velde <remco.pc@outlook.com>
2022-07-31 23:13:48 +0200

    bugfix core_exec

--- src/Cli/Doctrine/View/Doctrine.Orm.generate-proxies.tpl
+++ src/Cli/Doctrine/View/Doctrine.Orm.generate-proxies.tpl
@@ -1,8 +1,8 @@
 {{R3M}}
 {{$is.set  = dir.set(config('project.dir.root'))}}
 {{core.exec('vendor/bin/doctrine orm:generate-proxies', 'output')}}
-{{implode("\n", $output)}}
+{{$output)}}
 {{core.exec('chown www-data:www-data "/tmp" -R', 'output')}}
-{{implode("\n", $output)}}
+{{$output)}}
 
 
--- src/Plugin/Function_core_exec.php
+++ src/Plugin/Function_core_exec.php
@@ -17,10 +17,13 @@ use R3m\Io\Module\Core;
 function function_core_exec(Parse $parse, Data $data, $command, $attribute=null, $type=null){
     $object = $parse->object();
     $output = [];
-    Core::execute($command, $output, $type);
+    Core::execute($command, $output, $error, $type);
     if($attribute) {
         if (substr($attribute, 0, 1) === '$') {
             $attribute = substr($attribute, 1);
+        }
+        if($error){
+            $data->data($attribute . '_error', $error);
         }
         $data->data($attribute, $output);
     }
