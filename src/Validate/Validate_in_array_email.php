8017935691e753658f5d59ae38a83185715271d2
Remco van der Velde <remco.pc@outlook.com>
2022-08-01 19:32:58 +0200

    Create Validate_in_array_email.php

---
+++ src/Validate/Validate_in_array_email.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-09-18
+ * @copyright       Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *     -            all
+ */
+use R3m\Io\Module\Parse\Token;
+
+function validate_in_array_email(R3m\Io\App $object, $field='', $argument=''){
+    if($object->request('has', 'node.' . $field)){
+        $array = $object->request('node.' . $field);
+    } else {
+        $array = $object->request($field);
+    }
+    foreach($array as $nr => $value){
+        if(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            // invalid address
+            return false;
+        }
+    }
+    return true;
+}
