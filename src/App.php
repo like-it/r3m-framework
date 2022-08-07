6d6d655819d13103deb64a51e1690562b2757dad
Remco van der Velde <remco.pc@outlook.com>
2022-08-07 02:16:12 +0200

    Update App.php

--- src/App.php
+++ src/App.php
@@ -511,7 +511,6 @@ class App extends Data {
     public static function instance($configuration=[]): App
     {
         $dir_vendor = Dir::name(__DIR__, 3);
-        dd($dir_vendor);
         $autoload = $dir_vendor . 'autoload.php';
         $autoload = require $autoload;
         $config = new Config([
