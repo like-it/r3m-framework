ce59c01e1e6ae950b09da0089b378dc3ae455fba
Remco van der Velde <remco.pc@outlook.com>
2022-08-26 16:39:52 +0200

    Update TypeUuid.php
    bugfix good uiid = false

--- src/Module/Route/TypeUuid.php
+++ src/Module/Route/TypeUuid.php
@@ -18,36 +18,28 @@ class TypeUuid {
     public static function validate(App $object, $string=''): bool
     {
         //format: %s%s-%s-%s-%s-%s%s%s
-        d($string);
-        if(strlen($string) === 36){
+        if(strlen($string) !== 36){
             return false;
         }
         $explode = explode('-', $string);
         if (count($explode) !== 5) {
-            d('1');
             return false;
         }
         if (strlen($explode[0]) !== 8) {
-            d('2');
             return false;
         }
         if (strlen($explode[1]) !== 4) {
-            d('3');
             return false;
         }
         if (strlen($explode[2]) !== 4) {
-            d('4');
             return false;
         }
         if (strlen($explode[3]) !== 4) {
-            d('5');
             return false;
         }
         if (strlen($explode[4]) !== 12) {
-            d('6');
             return false;
         }
-        dd('test');
         return true;
     }
 
