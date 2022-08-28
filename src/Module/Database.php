a87ac7c714ec224ffcbd082cc0569322f9f37a49
Remco van der Velde <remco.pc@outlook.com>
2022-08-28 19:26:39 +0200

    Update Database.php

--- src/Module/Database.php
+++ src/Module/Database.php
@@ -127,7 +127,7 @@ class Database {
             $logger->pushHandler(new StreamHandler($object->config('project.dir.log') . 'sql.log', Logger::DEBUG));
             $logger->pushProcessor(new PsrLogMessageProcessor(null, true));
 
-            $logger->info('Logger initialized...');
+            $logger->info('Logger initialized...' . rand(1000, 9999));
 
             $configuration = $em->getConnection()->getConfiguration();
             $configuration->setMiddlewares([new Logging\Middleware($logger)]);
