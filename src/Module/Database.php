164b3c58400e86cc572e411af9fac0c95179c327
Remco van der Velde <remco.pc@outlook.com>
2022-09-04 22:52:10 +0200

    Update Database.php

--- src/Module/Database.php
+++ src/Module/Database.php
@@ -105,7 +105,8 @@ class Database {
      * @throws ORMException2
      * @throws Exception
      */
-    public static function entityManager(App $object, $options=[]){
+    public static function entityManager(App $object, $options=[]): ?EntityManager
+    {
         $environment = $object->config('framework.environment');
         if(empty($environment)){
             $environment = Config::MODE_DEVELOPMENT;
@@ -117,7 +118,6 @@ class Database {
         if(!empty($entityManager)){
             return $entityManager;
         }
-
         $url = $object->config('project.dir.data') . 'Config.json';
         $config  = $object->parse_read($url, sha1($url));
         if($config){
@@ -126,16 +126,19 @@ class Database {
             $proxyDir = $config->get('doctrine.proxy.dir');
             $cache = null;
             $config = ORMSetup::createAnnotationMetadataConfiguration($paths, false, $proxyDir, $cache);
-            $logger = new Logger(Database::LOGGER_DOCTRINE);
-            $logger->pushHandler(new StreamHandler($object->config('project.dir.log') . 'sql.log', Logger::DEBUG));
-            $logger->pushProcessor(new PsrLogMessageProcessor(null, true));
-            $object->logger($logger->getName(), $logger);
-            $logger->info('Logger initialized...');
-            $config->setMiddlewares([new Logging\Middleware($logger)]);
+            if(!empty($connection['logging'])){
+                $logger = new Logger(Database::LOGGER_DOCTRINE);
+                $logger->pushHandler(new StreamHandler($object->config('project.dir.log') . 'sql.log', Logger::DEBUG));
+                $logger->pushProcessor(new PsrLogMessageProcessor(null, true));
+                $object->logger($logger->getName(), $logger);
+                $logger->info('Logger initialized...');
+                $config->setMiddlewares([new Logging\Middleware($logger)]);
+            }
             $connection = DriverManager::getConnection($connection, $config, new EventManager());
             $em = EntityManager::create($connection, $config);
             $object->set(Database::NAME .'.entityManager. ' . $environment, $em);
             return $em;
         }
+        return null;
     }
 }
