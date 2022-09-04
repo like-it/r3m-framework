dcbb3065328bbe290867eb572d7476d69eb8dd22
Remco van der Velde <remco.pc@outlook.com>
2022-09-04 22:17:00 +0200

    Update Database.php

--- src/Module/Database.php
+++ src/Module/Database.php
@@ -135,9 +135,10 @@ class Database {
 
             $config->setMiddlewares([new Logging\Middleware($logger)]);
             $connection = DriverManager::getConnection($connection, $config, new EventManager());
-            $logging_connection = new Logging\Connection($connection, $logger);
             $em = EntityManager::create($connection, $config);
             $debug_stack = new DebugStack();
+//            $logging_connection = new Logging\Connection($connection, $logger);
+//            dd(get_class($connection));
             $em->getConnection()->getConfiguration()->setSQLLogger($debug_stack);
 
 
