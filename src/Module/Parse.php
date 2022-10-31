6fe51339e22ec244572b1586a827d838eadb5a28
Remco van der Velde <remco.pc@outlook.com>
2022-11-01 00:18:30 +0100

    Update Parse.php

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -379,7 +379,6 @@ class Parse {
             if($exists){
                 $template = new $class(new Parse($this->object()), $storage);
                 $string = $template->run();
-                d($string);
                 if(empty($this->halt_literal())){
                     $string = Literal::restore($storage, $string);
                 }
