b32efa2f2a7b370642c2783cc793d9a187acb5a0
Remco van der Velde <remco.pc@outlook.com>
2022-11-01 14:19:41 +0100

    Update Parse.php

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -295,6 +295,7 @@ class Parse {
                         if($index === 0){
                             $storage->data($key, clone $rootNode);
                         } else {
+                            d($this->local($index));
                             $storage->data($key, $this->local($index));
                         }
                     }
