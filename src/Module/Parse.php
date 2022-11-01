7f8a4a7bcb9a3a30575827981ef15b69dcd9b305
Remco van der Velde <remco.pc@outlook.com>
2022-11-01 14:01:47 +0100

    Update Parse.php

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -405,6 +405,7 @@ class Parse {
                 }
                 $storage->data('delete', 'this');
             } else {
+                //add phpstan error report on class in /tmp/r3m/io/parse/error/...
                 throw new Exception('Class ('. $class .') doesn\'t exist');
 
             }
