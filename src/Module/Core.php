d61c6bfb15a5406f5d5302f430ac511c506fa000
Remco van der Velde <remco.pc@outlook.com>
2022-11-04 23:39:18 +0100

    Update Core.php

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -769,13 +769,12 @@ class Core {
             if(Core::cors_is_allowed($object, $origin)){
                 header("Access-Control-Allow-Origin: {$origin}");
             }
-        } else {
-            d($_SERVER);
         }
         if (
             array_key_exists('REQUEST_METHOD', $_SERVER) &&
             $_SERVER['REQUEST_METHOD'] == 'OPTIONS'
         ) {
+            d($_SERVER);
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
             //header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
