ef5e40b6e0d875e73b962d26e893e5e821214c3e
Remco van der Velde <remco.pc@outlook.com>
2022-11-04 23:13:45 +0100

    Update Core.php

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -727,6 +727,7 @@ class Core {
         header("Access-Control-Allow-Origin: *");
         if (array_key_exists('HTTP_ORIGIN', $_SERVER)) {
             header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            ddd($_SERVER);
         }
         if (
             array_key_exists('REQUEST_METHOD', $_SERVER) &&
