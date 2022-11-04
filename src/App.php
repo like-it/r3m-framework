bec82785f293ffa450abf706b741283e845b926c
Remco van der Velde <remco.pc@outlook.com>
2022-11-04 23:11:12 +0100

    cors

--- src/App.php
+++ src/App.php
@@ -103,6 +103,7 @@ class App extends Data {
      */
     public static function run(App $object){
         if(App::is_cli() === false){
+            ddd($_SERVER);
             Core::cors($object);
         }
         //Config::configure($object); //@moved to construct
--- src/Module/Core.php
+++ src/Module/Core.php
@@ -732,7 +732,6 @@ class Core {
             array_key_exists('REQUEST_METHOD', $_SERVER) &&
             $_SERVER['REQUEST_METHOD'] == 'OPTIONS'
         ) {
-            ddd($_SERVER);
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
             //header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
