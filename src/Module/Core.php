6a721023b8f73a403b8be96073f6303e890bec41
Remco van der Velde <remco.pc@outlook.com>
2022-11-06 21:19:17 +0100

    Update Core.php

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -744,8 +744,6 @@ class Core {
                             $temp[0] = '';
                             $host = implode('.', $explode);
                             $match = implode('.', $temp);
-                            d($host);
-                            d($match);
                             if($host === $match){
                                 return true;
                             }
