a90c0bbcb6e8f987428c0ff7704322f1fee57260
Remco van der Velde <remco.pc@outlook.com>
2022-11-10 00:12:35 +0100

    Update Function_domain_add.php

--- src/Cli/Configure/Plugin/Function_domain_add.php
+++ src/Cli/Configure/Plugin/Function_domain_add.php
@@ -125,6 +125,8 @@ function function_domain_add(Parse $parse, Data $data, $domain=''){
         try {
             File::write($url, Core::object($route->data(), Core::OBJECT_JSON));
             File::chmod($url, 0666);
+            Core::execute('chown www-data:www-data -R ' . $host_dir_root);
+            Core::execute('chmod 777 -R ' . $host_dir_root);
         } catch (Exception | FileWriteException | ObjectException $exception){
             return $exception->getMessage() . PHP_EOL;
         }
@@ -241,11 +243,11 @@ function function_domain_add(Parse $parse, Data $data, $domain=''){
         try {
             File::write($url, Core::object($route->data(), Core::OBJECT_JSON));
             File::chmod($url, 0666);
+            Core::execute('chown www-data:www-data -R ' . $host_dir_root);
+            Core::execute('chmod 777 -R ' . $host_dir_root);
         } catch (Exception | FileWriteException | ObjectException $exception){
             return $exception->getMessage() . "\n";
         }
     }
-    Core::execute('chown www-data:www-data -R ' . $host_dir_root);
-    Core::execute('chmod 777 -R ' . $host_dir_root);
 }
 
