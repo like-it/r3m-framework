fabb15fdf13dc695f0351b00fd64aff25804b202
Remco van der Velde <remco.pc@outlook.com>
2022-11-09 23:54:30 +0100

    Update Function_route_restart.php

--- src/Cli/Route/Plugin/Function_route_restart.php
+++ src/Cli/Route/Plugin/Function_route_restart.php
@@ -8,8 +8,15 @@ function function_route_restart(Parse $parse, Data $data){
     $object = $parse->object();
     $route = $object->data(\R3m\Io\App::ROUTE);
     $cache_url = $route->cache_url();
-    d($cache_url);
     if(File::exist($cache_url)){
         File::delete($cache_url);
+    }
+    $cache_url_2 = str_replace('Cache/0', 'Cache/33', $cache_url);
+    if(File::exist($cache_url_2)){
+        File::delete($cache_url_2);
+    }
+    $cache_url_3 = str_replace('Cache/0', 'Cache/1000', $cache_url);
+    if(File::exist($cache_url_3)){
+        File::delete($cache_url_3);
     }
 }
