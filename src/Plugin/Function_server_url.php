24abb1a9f4962f371fdbb5af64788ae5b2f2a25e
Remco van der Velde <remco.pc@outlook.com>
2022-11-20 22:25:38 +0100

    Update Function_server_url.php

--- src/Plugin/Function_server_url.php
+++ src/Plugin/Function_server_url.php
@@ -7,11 +7,5 @@ use R3m\Io\Config;
 function function_server_url(Parse $parse, Data $data, $name=''){
     $object = $parse->object();
     $name = str_replace('.', '-', $name);
-    $url =  $object->config('server.url.' . $name . '.' . $object->config('framework.environment'));
-    d($url);
-    if(substr($url, 0, 2) === '{{' && substr($url, -2, 2) === '}}'){
-        d($data);
-        d($parse->data());
-    }
-    return $url;
+    return $object->config('server.url.' . $name . '.' . $object->config('framework.environment'));
 }
