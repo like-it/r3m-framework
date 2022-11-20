41f25c1bb3d7d845c102b52f873b16f76168bbec
Remco van der Velde <remco.pc@outlook.com>
2022-11-20 22:16:01 +0100

    Update Function_server_url.php

--- src/Plugin/Function_server_url.php
+++ src/Plugin/Function_server_url.php
@@ -8,6 +8,7 @@ function function_server_url(Parse $parse, Data $data, $name=''){
     $object = $parse->object();
     $name = str_replace('.', '-', $name);
     $url =  $object->config('server.url.' . $name . '.' . $object->config('framework.environment'));
+    d($url);
     if(substr($url, 0, 2) === '{{' && substr($url, -2, 2) === '}}'){
         d($data);
         d($parse->data());
