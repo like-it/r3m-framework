450debc6a8a83e2dd8afe808187893c82bb48992
Remco van der Velde <remco.pc@outlook.com>
2022-11-24 18:18:33 +0100

    Create Function_route_current.php

---
+++ src/Plugin/Function_route_current.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2022-11-24
+ * @copyright       Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *     -            all
+ */
+use R3m\Io\Module\Parse;
+use R3m\Io\Module\Data;
+
+function function_route_current(Parse $parse, Data $data){
+    $object = $parse->object();
+    ddd($object->route());
+    $url = $object->route();//find($object, $name, $options);
+    return $url;
+}
