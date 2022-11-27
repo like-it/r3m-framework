1e5fe116ccc5860b2ac0b1b521dc0532be3b8871
Remco van der Velde <remco.pc@outlook.com>
2022-11-27 22:08:36 +0100

    cors enable / disable

---
+++ src/Cli/Configure/Plugin/Function_cors_disable.php
@@ -0,0 +1,50 @@
+<?php
+
+use R3m\Io\Module\Parse;
+use R3m\Io\Module\Data;
+use R3m\Io\Module\File;
+
+use Exception;
+use R3m\Io\Exception\FileAppendException;
+
+/**
+ * @throws Exception
+ */
+function function_cors_disable(Parse $parse, Data $data, $host=''){
+    $id = posix_geteuid();
+    if(
+        !in_array(
+            $id,
+            [
+                0,
+                33
+            ]
+        )
+    ){
+        throw new Exception('Only root & www-data can configure cors enable...');
+    }
+    if(empty($host)){
+        throw new Exception('Host cannot be empty...');
+    }
+    $object = $parse->object();
+    $url = $object->config('project.dir.data') . 'Config' . $object->config('extension.json');
+    $config = $object->data_read($url);
+    if(!$config){
+        $config = new Data();
+    }
+    $list = $config->get('server.cors');
+    if(empty($list)){
+        $list = [];
+    }
+    if(in_array($host, $list)){
+        foreach($list as $key => $value){
+            if($value === $host){
+                unset($list[$key]);
+            }
+        }
+    }
+    $config->set('server.cors', $list);
+    $config->write($url);
+    return 'Cors disabled for host: ' . $host . PHP_EOL;
+}
+
