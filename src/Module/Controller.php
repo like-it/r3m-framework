e215c3aa91a74bc254bd73067ce6f280704cc47f
Remco van der Velde <remco.pc@outlook.com>
2022-12-12 15:11:08 +0100

    Update Controller.php

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -127,7 +127,7 @@ class Controller {
             foreach($list as $nr => $file){
                 $result[] = $file;
             }
-            ddd($result);
+            ddd($list);
             if($config->data(Config::DATA_FRAMEWORK_ENVIRONMENT) === Config::MODE_DEVELOPMENT){
                 throw new LocateException('Cannot find view file', $result, 1);
             } else {
