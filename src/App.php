3c46883c72465e9a608c132c99cb231317634fad
Remco van der Velde <remco.pc@outlook.com>
2023-01-01 23:46:05 +0100

    Update App.php

--- src/App.php
+++ src/App.php
@@ -232,6 +232,7 @@ class App extends Data {
                             $request
                         );
                         $result = $route->controller::{$route->function}($object);
+                        d($result);
                     } else {
                         $object->logger(App::LOGGER_NAME)->error(
                             'Controller (' .
