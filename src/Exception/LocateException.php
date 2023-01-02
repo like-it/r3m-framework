69c171ec7cc3a1acda395dd34f5e8aec1a59a69a
Remco van der Velde <remco.pc@outlook.com>
2023-01-02 01:00:43 +0100

    Update LocateException.php

--- src/Exception/LocateException.php
+++ src/Exception/LocateException.php
@@ -60,7 +60,7 @@ class LocateException extends Exception {
     {
         $object = $this->object();
         if($object){
-            $object->config('exception.locate', '{{config(\'project.dir.host\')}}{{string.uppercase.first(host.subdomain())}}/{{string.uppercase.first(host.domain())}}/{{string.uppercase.first(host.extension())}/View/Exception/Locate.tpl');
+            $object->config('exception.locate', '{{config(\'project.dir.host\')}}{{string.uppercase.first(host.subdomain())}}/{{string.uppercase.first(host.domain())}}/{{string.uppercase.first(host.extension())}}/View/Exception/Locate.tpl');
             $object->set('exception.message', $this->getMessage());
             $object->set('exception.code', $this->getCode());
             $object->set('exception.file', $this->getFile());
