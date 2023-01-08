596acde5c2c194383461d4662a880a18c2ad8c6e
Remco van der Velde <remco.pc@outlook.com>
2023-01-08 22:10:50 +0100

    Update Controller.php

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -458,6 +458,7 @@ class Controller {
                 $data->rdelim = Controller::RDELIM;
             }
         }
+        $object->config('response.output', Response::TYPE_HTML);
         $read = $parse->compile($read, $data, $parse->storage());
         Parse::readback($object, $parse, App::SCRIPT);
         Parse::readback($object, $parse, App::LINK);
