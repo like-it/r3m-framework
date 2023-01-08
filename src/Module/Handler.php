a727b745208c1f96990de6a7e07e448bd72585d1
Remco van der Velde <remco.pc@outlook.com>
2023-01-08 21:56:09 +0100

    Update Handler.php

--- src/Module/Handler.php
+++ src/Module/Handler.php
@@ -139,6 +139,7 @@ class Handler {
             is_string($http_response_code)
         ){
             $list = headers_list();
+            ddd($list);
             $header_list = [];
             foreach($list as $nr => $record){
                 $tmp = explode(':', $record, 2);
