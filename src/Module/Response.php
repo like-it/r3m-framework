58c88872298e150e43c473d23d9a870b16c59491
Remco van der Velde <remco.pc@outlook.com>
2023-01-08 21:52:26 +0100

    Update Response.php

--- src/Module/Response.php
+++ src/Module/Response.php
@@ -75,7 +75,9 @@ class Response {
                         break;
                 }
             }
-            $header = $object->config('response.' . $type .'.header');
+            $contentType = Handler::header('get', 'Content-Type');
+            d($contentType);
+            $header = $object->config('response.' . $contentType .'.header');
             if(
                 !empty($header) &&
                 is_array($header)
