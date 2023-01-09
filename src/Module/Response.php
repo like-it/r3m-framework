273774f7ab09a114db04b84318a06a761cf41d73
Remco van der Velde <remco.pc@outlook.com>
2023-01-09 15:18:21 +0100

    Update Response.php

--- src/Module/Response.php
+++ src/Module/Response.php
@@ -161,7 +161,6 @@ class Response {
             case Response::TYPE_FILE :
             case Response::TYPE_HTML :
             case Response::TYPE_XML :
-                ddd(headers_list());
                 return $response->data();
         }
     }
