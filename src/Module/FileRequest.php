03586a721044ae87fcc737f7c9dfd511decadb6c
Remco van der Velde <remco.pc@outlook.com>
2023-01-16 00:40:08 +0100

    Update FileRequest.php

--- src/Module/FileRequest.php
+++ src/Module/FileRequest.php
@@ -248,6 +248,8 @@ class FileRequest {
             }
         }
         Handler::header('HTTP/1.0 404 Not Found', 404);
+        d($extension);
+        ddd($config->get('error.extension.tpl'));
         if($config->data('framework.environment') === Config::MODE_DEVELOPMENT){
             throw new LocateException('Cannot find location for file:' . "<br>\n" . implode("<br>\n", $location), $location);
         } else {
