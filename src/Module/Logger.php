2210d43680482eba8215d3741b8ab956b282b82d
Remco van der Velde <remco.pc@outlook.com>
2023-01-16 15:52:56 +0100

    Update Logger.php

--- src/Module/Logger.php
+++ src/Module/Logger.php
@@ -17,12 +17,13 @@ use Exception;
 
 class Logger {
 
+    const FILEREQUEST = 'FileRequest';
     /**
      * @throws Exception
      */
     public static function configure(App $object){
         $interface = $object->config('log');
-        $object->config('project.log.file', File::TYPE);
+        $object->config('project.log.fileRequest', Logger::FILEREQUEST);
         if($interface){
             foreach($interface as $name => $record){
                 $name = ucfirst($name);
