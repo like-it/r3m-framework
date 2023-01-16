7c8533f6db8cc465a141932f06414483828404a1
Remco van der Velde <remco.pc@outlook.com>
2023-01-16 13:22:43 +0100

    Update Core.php

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -796,6 +796,7 @@ class Core {
         if (array_key_exists('HTTP_ORIGIN', $_SERVER)) {
             $origin = $_SERVER['HTTP_ORIGIN'];
             if(Core::cors_is_allowed($object, $origin)){
+                $origin = rtrim($_SERVER['HTTP_REFERER'], '/');
                 header("Access-Control-Allow-Origin: {$origin}");
                 header('Access-Control-Allow-Credentials: true');
             }
