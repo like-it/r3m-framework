7be0faf5e93310d5a71dffa1d7b763620b4fe165
Remco van der Velde <remco.pc@outlook.com>
2023-01-25 01:44:43 +0100

    Update Controller.php

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -438,7 +438,7 @@ class Controller {
      * @throws UrlNotExistException
      * @throws FileWriteException
      */
-    public static function response(App $object, $url, $data=null)
+    public static function response(App $object, $url, $data=null): string
     {
         if(empty($url)){
             throw new UrlEmptyException('Url is empty');
@@ -479,6 +479,10 @@ class Controller {
         $read = $parse->compile($read, $data, $parse->storage());
         Parse::readback($object, $parse, App::SCRIPT);
         Parse::readback($object, $parse, App::LINK);
+        if(is_array($read)){
+            $debug = debug_backtrace(true);
+            ddd($debug);
+        }
         return $read;
     }
 
