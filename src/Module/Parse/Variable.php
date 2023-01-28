136676a48ee8f85f840896f4ba9c2ec6af7c3047
Remco van der Velde <remco.pc@outlook.com>
2023-01-29 00:50:44 +0100

    Update Variable.php

--- src/Module/Parse/Variable.php
+++ src/Module/Parse/Variable.php
@@ -70,9 +70,6 @@ class Variable {
                     if(array_key_exists('execute', $record)){
                         $execute[] = $record['execute'];
                     }
-                    elseif($record['is_operator'] === true){
-                        $execute[] = $record['value'];
-                    }
                     else {
                         if(
                             array_key_exists('type', $record) &&
@@ -85,7 +82,19 @@ class Variable {
                             $tree = $build->require('modifier', $tree);
                             $tree = $build->require('function', $tree);
                             $execute[] = Value::get($build, $storage, reset($tree));
-                        } else {
+                        }
+                        elseif(
+                            array_key_exists('type', $record) &&
+                            $record['type'] === Token::TYPE_METHOD &&
+                            array_key_exists('method', $record)
+                        ){
+                            $tree = [];
+                            $tree[]= $record;
+                            $tree = $build->require('modifier', $tree);
+                            $tree = $build->require('function', $tree);
+                            $execute[] = Value::get($build, $storage, reset($tree));
+                        }
+                        else {
                             d($record);
                         }
                     }
