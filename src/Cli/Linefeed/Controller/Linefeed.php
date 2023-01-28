dbc4832c9d1957897ffcb9fc42e16858c9204641
Remco van der Velde <remco.pc@outlook.com>
2023-01-28 02:33:49 +0100

    Update Linefeed.php

--- src/Cli/Linefeed/Controller/Linefeed.php
+++ src/Cli/Linefeed/Controller/Linefeed.php
@@ -14,13 +14,13 @@ use R3m\Io\App;
 use R3m\Io\Module\Cli;
 use R3m\Io\Module\Dir;
 use R3m\Io\Module\File;
-use R3m\Io\Module\View;
+use R3m\Io\Module\Controller;
 use Exception;
 use R3m\Io\Exception\LocateException;
 use R3m\Io\Exception\UrlEmptyException;
 use R3m\Io\Exception\UrlNotExistException;
 
-class Linefeed extends View {
+class Linefeed extends Controller {
     const DIR = __DIR__;
     const NAME = 'Linefeed';
     const INFO = '{{binary()}} linefeed                       | Linefeed';
