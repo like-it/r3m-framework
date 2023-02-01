22e9e5073bdf7448d6b0e64bdda10396b986a182
Remco van der Velde <remco.pc@outlook.com>
2023-02-01 22:30:06 +0100

    Update Variable.php

--- src/Module/Parse/Variable.php
+++ src/Module/Parse/Variable.php
@@ -377,7 +377,7 @@ class Variable {
             array_key_exists('has_modifier', $variable['variable']) &&
             $variable['variable']['has_modifier'] === true
         ){
-            ddd($variable);
+            d($variable);
             foreach($variable['variable']['modifier'] as $nr => $modifier_list){
                 foreach($modifier_list as $modifier_nr => $modifier){
                     if(!array_key_exists('php_name', $modifier)){
