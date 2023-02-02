4529aede538e9ec5dc4964fdd7b0d96123f04483
Remco van der Velde <remco.pc@outlook.com>
2023-02-02 17:13:47 +0100

    Update Method.php

--- src/Module/Parse/Method.php
+++ src/Module/Parse/Method.php
@@ -153,7 +153,7 @@ class Method {
             } else {                
                 foreach($record['method']['attribute'] as $nr => $token){
                     $token = $build->require('function', $token);
-                    $value = Variable::getValue($build, $storage, $token, false,$extra);
+                    $value = Variable::getValue($build, $storage, $token);
                     $attribute .= $value . ', ';
                 }
             }
