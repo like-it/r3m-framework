0e31cf02fdb0718ee9f794478ca293c0e6ab165c
Remco van der Velde <remco.pc@outlook.com>
2023-02-12 23:29:50 +0100

    Update Core.php

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -157,14 +157,13 @@ class Core
                 1 => array("pipe", "w"),  // stdout
                 2 => array("pipe", "w"),  // stderr
             );
-            ob_flush();
             $process = proc_open($command, $descriptorspec, $pipes, Dir::current(), null);
             $output = stream_get_contents($pipes[1]);
             fclose($pipes[1]);
 
             $error = stream_get_contents($pipes[2]);
             fclose($pipes[2]);
-
+            ob_flush();
             return proc_close($process);
         }
     }
