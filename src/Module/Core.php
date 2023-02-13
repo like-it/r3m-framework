bf971a2457a6c987e90f7ad75fda82599eff0512
Remco van der Velde <remco.pc@outlook.com>
2023-02-13 02:44:48 +0100

    Update Core.php

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -173,10 +173,7 @@ class Core
                 1 => STDOUT,  // stdout
                 2 => array("pipe", "w"),  // stderr
             );
-
-            ob_start();
             $process = proc_open($command, $descriptorspec, $pipes, Dir::current(), null);
-            $output = ob_get_contents();
 //            stream_set_blocking($pipes[1], 0);
 //            stream_set_blocking($pipes[2], 0);
 //            stream_set_blocking(STDIN, 0);
