61b59df253d67ecc7df2843e681ece2098c0a7be
Remco van der Velde <remco.pc@outlook.com>
2023-02-13 02:02:58 +0100

    Update Core.php

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -191,7 +191,7 @@ class Core
                     $write = NULL;
                     $except = NULL;
                     $tv = 0;
-                    $utv = 5000000;
+                    $utv = 500000;
 
                     $n = stream_select($read, $write, $except, $tv, $utv);
                     if($n > 0) {
