25b6c899a372ca07061b93628d5b8fceaff5d663
Remco van der Velde <remco.pc@outlook.com>
2023-02-15 22:43:06 +0100

    Update Variable.php

--- src/Module/Parse/Variable.php
+++ src/Module/Parse/Variable.php
@@ -118,7 +118,6 @@ class Variable {
         $result = '\'' . $variable['variable']['attribute'];
         $quote_add = false;
         $add_quote = false;
-        d($execute);
         foreach($execute as $nr => $record){
             if(substr($record, 0, 2) === '[]'){
                 $result .= substr($record, 0, 2);
