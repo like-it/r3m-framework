f5f4ddbd16530ab1ca3d29bff90384f090681904
Remco van der Velde <remco.pc@outlook.com>
2023-02-16 01:09:22 +0100

    Update Data.php

--- src/Module/Data.php
+++ src/Module/Data.php
@@ -252,6 +252,7 @@ class Data {
                     $attribute = (string) $attribute;
                 }
                 if(is_string($attribute)){
+                    d($attribute);
                     return Core::object_get($attribute, $this->data());
                 }
                 elseif(is_object($attribute) && get_class($attribute) === Data::class){
