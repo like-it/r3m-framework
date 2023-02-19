b1ab2cb0448166986ba1efb7f7ea4e5a04c679f7
Remco van der Velde <remco.pc@outlook.com>
2023-02-19 03:57:57 +0100

    Update Core.php

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -236,7 +236,7 @@ class Core
                     echo $command . PHP_EOL;
                     $process = proc_open($command, $descriptorspec, $pipes, Dir::current(), null);
 //                    $error = stream_get_contents($pipes[2]);
-                    fclose($pipes[2]);
+//                    fclose($pipes[2]);
                     return proc_close($process);
             }
         }
