5b6e7061800fc8d15ae4a61eeaaaaa0676fc64c5
Remco van der Velde <remco.pc@outlook.com>
2023-03-02 00:32:15 +0100

    Update Function_object_select.php

--- src/Plugin/Function_object_select.php
+++ src/Plugin/Function_object_select.php
@@ -18,5 +18,6 @@ use R3m\Io\Module\Core;
  * @throws \R3m\Io\Exception\FileWriteException
  */
 function function_object_select(Parse $parse, Data $data, $url, $select=null, $compile=false, $scope='scope:object'){
+    d($select);
     return Core::object_select($parse, $data, $url, $select, $compile, $scope);
 }
