9ae18097e31117f91d7dc51f1effd32d7505c4cb
Remco van der Velde <remco.pc@outlook.com>
2023-03-13 01:30:56 +0100

    Update Autoload.php

--- src/Module/Autoload.php
+++ src/Module/Autoload.php
@@ -161,6 +161,7 @@ class Autoload {
         }
         $parameters = Config::parameters($object, $parameters);
         $cache_dir = $parameters['cache'];
+        ddd($cache_dir);
         $autoload->cache_dir($cache_dir);
         $autoload->register();
         $autoload->environment($object->config('framework.environment'));
