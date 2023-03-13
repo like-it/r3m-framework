dc21f87f9aa7898b7d4e53f7a8fb3ed02243a5b4
Remco van der Velde <remco.pc@outlook.com>
2023-03-13 23:59:09 +0100

    Update Function_ramdisk_mount.php

--- src/Plugin/Function_ramdisk_mount.php
+++ src/Plugin/Function_ramdisk_mount.php
@@ -46,7 +46,7 @@ function function_ramdisk_mount(Parse $parse, Data $data, $size='1G', $url='', $
     $command = 'chown www-data:www-data ' . $url . ' -R';
     Core::execute($object, $command);
     $mount_url = substr($url, 0, -1);
-    $command = 'mount -t tmpfs -o size=' . $size . ' ' . $name .' ' . $mount_url;
+    $command = 'mount -t tmpfs -o size=' . $size . ' "' . $name .'" "' . $mount_url .'"';
     echo $command . PHP_EOL;
     Core::execute($object, $command, $output);
     echo $output . PHP_EOL;
