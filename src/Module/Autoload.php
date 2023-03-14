2657384dd3460c4282a801155b03bc01f41d6059
Remco van der Velde <remco.pc@outlook.com>
2023-03-14 02:34:58 +0100

    Update Autoload.php

--- src/Module/Autoload.php
+++ src/Module/Autoload.php
@@ -79,7 +79,8 @@ class Autoload {
         if($cache_dir){
             $class_dir = $object->config('ramdisk.url') . 'Class' . $object->config('ds');
             $object->config('autoload.cache.class', $class_dir);
-            if(!is_dir($class_dir)){
+            if(!is_dir(substr($class_dir, 0, -1))){
+                ddd($class_dir);
                 mkdir($class_dir, 0750, true);
                 $id = posix_geteuid();
                 if(empty($id)){
