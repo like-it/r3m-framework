1760c4c01802be216ff44df1cbc97a620f3ebf9f
Remco van der Velde <remco.pc@outlook.com>
2023-03-19 20:22:36 +0100

    Update Autoload.php

--- src/Module/Autoload.php
+++ src/Module/Autoload.php
@@ -342,6 +342,7 @@ class Autoload {
             $load_directory = dirname($load);
             $load = basename($load) . '.' . Autoload::EXT_PHP;
             $load = Autoload::name_reducer($object, $load, $object->config('autoload.cache.file.max_length_file'),'_', 'shift');
+            d($load_directory);
             $load_directory = Autoload::name_reducer($object, $load_directory, $object->config('autoload.cache.file.max_length_directory'), $object->config('ds'), 'pop');
             d($load_directory);
             $load_url = $object->config('autoload.cache.class') . $load_directory . '_' . $load;
