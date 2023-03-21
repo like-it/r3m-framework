178e4b72274dfa05996b84d1aa97d11b9bdb9226
Remco van der Velde <remco.pc@outlook.com>
2023-03-21 18:29:28 +0100

    bugfix

--- src/Cli/Cache/Controller/Cache.php
+++ src/Cli/Cache/Controller/Cache.php
@@ -78,6 +78,7 @@ class Cache extends Controller {
 
     private static function clear(App $object){
         try {
+            $object->config('ramdisk.is.disabled', true);
             $name = Cache::name(__FUNCTION__, Cache::NAME);
             $url = Cache::locate($object, $name);
             return Cache::response($object, $url);
