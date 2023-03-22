9bf129ce7378e5421d4b2cd7e22f584d8d43c627
Remco van der Velde <remco.pc@outlook.com>
2023-03-22 11:25:11 +0100

    cache:clear the right way

--- src/Cli/Cache/Plugin/Function_cache_clear.php
+++ src/Cli/Cache/Plugin/Function_cache_clear.php
@@ -1,11 +1,12 @@
 <?php
 
+use R3m\Io\Config;
+
 use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
 use R3m\Io\Module\Dir;
 use R3m\Io\Module\Core;
 use R3m\Io\Module\File;
-use R3m\Io\Module\Route;
 
 function function_cache_clear(Parse $parse, Data $data){   
     $object = $parse->object();
@@ -22,11 +23,25 @@ function function_cache_clear(Parse $parse, Data $data){
         ob_flush();
     }
     if($read){
+        $id = $object->config(Config::POSIX_ID);
         foreach($read as $file){
             if($file->type === Dir::TYPE){
+                $file->number = false;
+                if(is_numeric($file->name)){
+                    $file->number = $file->name + 0;
+                }
                 if(
-                    is_numeric($file->name) &&
-                    file_exists($file->url)
+                    $file->number !== false &&
+                    file_exists($file->url) &&
+                    empty($id)
+                ){
+                    Dir::remove($file->url);
+                    echo 'Removed: ' . $file->url . PHP_EOL;
+                }
+                elseif(
+                    $file->number !== false &&
+                    file_exists($file->url) &&
+                    $id === $file->number
                 ){
                     Dir::remove($file->url);
                     echo 'Removed: ' . $file->url . PHP_EOL;
