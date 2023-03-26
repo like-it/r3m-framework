504211179eff262801c5c25a508035dccc27e153
Remco van der Velde <remco.pc@outlook.com>
2023-03-26 03:29:21 +0200

    added tpl

--- src/Exception/ParseException.php
+++ src/Exception/ParseException.php
@@ -117,6 +117,7 @@ class ParseException extends Exception {
         if(App::is_cli()){
             $output = [];
             $output[] = $string;
+            $output = [];
         } else {
             $output = [];
             $output[] = '<pre>';
