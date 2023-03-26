7119d82a8d81081a9211ea6d66435e9c8fb1ee43
Remco van der Velde <remco.pc@outlook.com>
2023-03-26 22:46:17 +0200

    IS_MAIL

--- src/App.php
+++ src/App.php
@@ -803,6 +803,15 @@ class App extends Data {
         }
     }
 
+    public static function is_mail() : bool
+    {
+        if(!defined('IS_MAIL')){
+            return Core::is_mail();
+        } else {
+            return true;
+        }
+    }
+
     /**
      * @throws Exception
      */
--- src/Exception/ParseException.php
+++ src/Exception/ParseException.php
@@ -75,7 +75,7 @@ class ParseException extends Exception {
                 for ($i = $line_nr - 5; $i <= $line_nr + 5; $i++) {
                     if (array_key_exists($i, $explode)) {
                         if($i === $line_nr - 1){
-                            if(!App::is_cli()){
+                            if(defined('IS_MAIL')){
                                 $explode[$i] = '<span class="selected">' . $explode[$i] . '</span>';
                             } else {
                                 $explode[$i] = Cli::color(['r'=> 200, 'g' => 0, 'b' => 0]) . $explode[$i] . Cli::tput('init');
@@ -94,42 +94,42 @@ class ParseException extends Exception {
         $string .= PHP_EOL .
             PHP_EOL
         ;
-        if(App::is_cli()){
+        if(defined('IS_MAIL')){
+            $string .= 'Code: ' . PHP_EOL;
+        } else {
             $title = 'Code: ';
             $width = Cli::width();
             $title_length = strlen($title);
             $width = $width - $title_length;
             $title .= str_repeat(' ', $width);
             $string .= Cli::color(null, ['r'=> 200, 'g' => 0, 'b' => 0]) . $title . Cli::tput('init') . PHP_EOL;
-        } else {
-            $string .= 'Code: ' . PHP_EOL;
         }
         $string .= implode(PHP_EOL, $result);
         if($source){
             $string .= PHP_EOL .
                 PHP_EOL
             ;
-            if(App::is_cli()){
+            if(defined('IS_MAIL')){
+                $string .= 'Source: ' . PHP_EOL;
+            } else {
                 $title = 'Source: ';
                 $width = Cli::width();
                 $title_length = strlen($title);
                 $width = $width - $title_length;
                 $title .= str_repeat(' ', $width);
                 $string .= Cli::color(null, ['r'=> 200, 'g' => 0, 'b' => 0]) . $title . Cli::tput('init') . PHP_EOL;
-            } else {
-                $string .= 'Source: ' . PHP_EOL;
             }
             $string .= $source;
         }
-        if(App::is_cli()){
-            $output = [];
-            $output[] = $string;
-            $output[] = '';
-        } else {
+        if(defined('IS_MAIL')){
             $output = [];
             $output[] = '<pre>';
             $output[] = $string;
             $output[] = '</pre>';
+        } else {
+            $output = [];
+            $output[] = $string;
+            $output[] = '';
         }
         return implode(PHP_EOL, $output);
     }
