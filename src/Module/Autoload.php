75ad365ebaea43feff5e2869a25b328398b74371
Remco van der Velde <remco.pc@outlook.com>
2023-03-31 15:38:48 +0200

    Update Autoload.php

--- src/Module/Autoload.php
+++ src/Module/Autoload.php
@@ -392,9 +392,12 @@ class Autoload {
                 $object->config('cache.autoload.url.directory_pop_or_shift')
             );
             $load_url = $object->config('autoload.cache.compile') . $load_directory . '_' . $load;
-            $data[] = $load_url;
-            $load_url = $object->config('autoload.cache.class') . $load_directory . '_' . $load;
-            $data[] = $load_url;
+            if(file_exists($load_url)){
+                $data[] = $load_url;
+            } else {
+                $load_url = $object->config('autoload.cache.class') . $load_directory . '_' . $load;
+                $data[] = $load_url;
+            }
             $object->config('autoload.cache.file.name', $load_url);
         }
         if(
