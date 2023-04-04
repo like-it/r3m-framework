037c18f387da39c1b8ad8b58a6b8e6ab5993fe70
Remco van der Velde <remco.pc@outlook.com>
2023-04-04 23:39:32 +0200

    Update Variable.php

--- src/Module/Parse/Variable.php
+++ src/Module/Parse/Variable.php
@@ -470,6 +470,7 @@ class Variable {
             elseif($is_collect === false){                                
                 $record = Method::get($build, $storage, $record);
                 $result .= Value::get($build, $storage, $record);
+                d($result)l
                 if(
                     !in_array(
                         $record['type'],
