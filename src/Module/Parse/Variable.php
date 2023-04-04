f4004990ec7b6e7a0d5eafbc57c2255ae63057d3
Remco van der Velde <remco.pc@outlook.com>
2023-04-04 23:26:12 +0200

    Update Variable.php

--- src/Module/Parse/Variable.php
+++ src/Module/Parse/Variable.php
@@ -509,7 +509,7 @@ class Variable {
         if(substr($result, -3) === ' . '){
             $result = substr($result,0, -3);
         }
-//        d($result);
+        d($result);
         return $result;
     }
 }
