3a671b69a9cd6f046e7764c15890c555ea4d7046
Remco van der Velde <remco.pc@outlook.com>
2023-04-05 13:35:08 +0200

    Update Method.php

--- src/Module/Parse/Method.php
+++ src/Module/Parse/Method.php
@@ -156,6 +156,8 @@ class Method {
                     $token = $build->require('function', $token);
                     $value = Variable::getValue($build, $storage, $token);
                     if(stristr($value, 'cli.autoload.run') !== false){
+                        $debug = debug_backtrace(true);
+                        ddd($debug);
                         d($token);
                         d($record);
                         ddd($value);
