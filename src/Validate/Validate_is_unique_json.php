4b44251240a41b0775e1bd82e49a8f506a8a695b
Remco van der Velde <remco.pc@outlook.com>
2023-04-06 18:30:09 +0200

    Update Validate_is_unique_json.php

--- src/Validate/Validate_is_unique_json.php
+++ src/Validate/Validate_is_unique_json.php
@@ -43,9 +43,9 @@ function validate_is_unique_json(R3m\Io\App $object, $string='', $field='', $arg
     $is_unique = true;
     if($url){
         $data = $object->data_read($url, sha1($url));
-        ddd($data);
         if($data){
             $result = $data->data($list);
+            ddd($result);
             if(is_array($result) || is_object($result)){
                 foreach($result as $nr => $record){
                     $uuid = false;
