05acc79c16866add0602d7eb2c41fd2e7981cef3
Remco van der Velde <remco.pc@outlook.com>
2023-04-07 21:39:26 +0200

    Update Operator.php

--- src/Module/Parse/Operator.php
+++ src/Module/Parse/Operator.php
@@ -194,6 +194,8 @@ class Operator {
                 unset($token[$nr]);
             }
         } else {
+            d($token);
+            d($statement);
             throw new exception('Statement must be an array in Operator::remove');
         }
         
