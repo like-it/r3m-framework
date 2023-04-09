8e6188d1dacc39118011fb7bd04c39fc24c90f56
Remco van der Velde <remco.pc@outlook.com>
2023-04-10 00:58:19 +0200

    Update App.php

--- src/App.php
+++ src/App.php
@@ -293,7 +293,7 @@ class App extends Data {
                         'route' => $route,
                         'type' => 'input'
                     ]);
-                    ddd(App::parameter($object, 'all', 1));
+                    ddd(App::parameter($object, 'all', 3));
                     ddd($route);
                     if (in_array($route->function, $methods, true)) {
                         $functions[] = $route->function;
