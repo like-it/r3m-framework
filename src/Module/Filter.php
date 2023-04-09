0049709086c360463cf276c626ff21b1294d2f91
Remco van der Velde <remco.pc@outlook.com>
2023-04-09 23:14:07 +0200

    Update Filter.php

--- src/Module/Filter.php
+++ src/Module/Filter.php
@@ -690,7 +690,6 @@ class Filter extends Data{
      * @throws Exception
      */
     public static function trigger(App $object, $action, $options=[]){
-        ddd($object->get(App::FILTER)->data());
         $filters = $object->get(App::FILTER)->select(Filter::NAME, [
             'action' => $action
         ]);
