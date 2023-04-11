f762ac3fe977f3ad2a1d18dd764fa4a612353099
Remco van der Velde <remco.pc@outlook.com>
2023-04-11 18:25:02 +0200

    Update Validate.php

--- src/Module/Validate.php
+++ src/Module/Validate.php
@@ -75,6 +75,7 @@ class Validate {
                 else {
                     $value = $object->request($field_request);
                 }
+                d($value);
                 if(
                     is_string($value) &&
                     substr($value, 0, 1) === '[' &&
