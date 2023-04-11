77d153a7fe18ab8cf4d3cdd98e4575bf4312362e
Remco van der Velde <remco.pc@outlook.com>
2023-04-11 17:23:45 +0200

    Update Validate_integer.php

--- src/Validate/Validate_integer.php
+++ src/Validate/Validate_integer.php
@@ -11,6 +11,7 @@
 use R3m\Io\Module\Parse\Token;
 
 function validate_integer(R3m\Io\App $object, $string='', $field='', $argument=''){
+    d($string);
     $int = intval($string);
     $argument = Token::tree('{if($argument ' . $argument . ')}{/if}');
     $left = null;
