07db9a97389abc48a57f19cd429a6c3c59017f62
Remco van der Velde <remco.pc@outlook.com>
2023-04-16 22:53:15 +0200

    Update Function_data_sort.php

--- src/Plugin/Function_data_sort.php
+++ src/Plugin/Function_data_sort.php
@@ -4,6 +4,6 @@ use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
 use R3m\Io\Module\Sort;
 
-function function_data_sort(Parse $parse, Data $data, $list, $options=[], $key_reset=false, $key=''){
-    return Sort::list($list)->with($options, $key_reset, $key);
+function function_data_sort(Parse $parse, Data $data, $list, $sort=[], $options=[]){
+    return Sort::list($list)->with($sort, $options);
 }
