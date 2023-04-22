fa3b85d1aeeb7c1d5f8eac16de87e7d1bbd6b8d9
remco van der Velde <remco.pc@outlook.com>
2023-04-22 11:53:16 +0200

    bug not parsing html

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -324,7 +324,7 @@ class Parse {
                     $attribute = $this->object()->config('parse.read.object.this.attribute');
                     $string->{$attribute} = $key;
                     $value = $this->compile($value, $storage->data(), $storage, $depth, $is_debug);
-                    if($key === 'html'){
+                    if($key === 'content'){
                         d($value);
                         ddd($string);
                     }
