b90589f9b021588f7fb43504177a1cc12b541e23
remco van der Velde <remco.pc@outlook.com>
2023-05-05 19:56:24 +0200

    validate upgrade

---
+++ src/Validate/String.Has.Dot.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-09-18
+ * @copyright       Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *     -            all
+ */
+use R3m\Io\App;
+use R3m\Io\Module\Parse\Token;
+
+/**
+ * @throws Exception
+ */
+function validate_string_has_dot(App $object, $string='', $field='', $argument=''): bool
+{
+    $split = str_split($string);
+    $test = [];
+    foreach($split as $nr => $char){
+        if(
+            in_array(
+                $char,
+                [
+                    '.',
+                ],
+                true
+            )
+        ){
+            $test[] = $char;
+        }
+    }
+    $length = count($test);
+    $argument = Token::tree('{if($argument ' . $argument . ')}{/if}');
+    $left = null;
+    $equation = null;
+    $right = null;
+    foreach($argument[1]['method']['attribute'][0] as $nr => $record){
+        if(empty($left)){
+            $left = $record;
+        }
+        elseif(empty($equation)){
+            $equation = $record['value'];            
+        }
+        elseif(empty($right)){
+            $right = $record['execute'];
+            break;
+        }
+    }
+    $result = false;
+    switch($equation){
+        case '>' :
+            $result = $length > $right;
+        break;
+        case '<' :
+            $result = $length < $right;
+        break;
+        case '>=' :
+            $result = $length >= $right;
+        break;
+        case '<=' :
+            $result = $length <= $right;
+        break;                
+        case '==' :
+            $result = $length == $right;
+        break;
+        case '!=' :
+            $result = $length != $right;
+        break;
+        case '===' :
+            $result = $length === $right;
+            break;
+        case '!==' :
+            $result = $length !== $right;
+            break;
+        default:
+            throw new Exception('Unknown equation');
+    }
+    return $result;    
+}
