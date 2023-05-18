dfde673461da490087e0def8bd96d838619266ca
remco van der Velde <remco.pc@outlook.com>
2023-05-18 10:21:58 +0200

    validate_in_array_uuid

--- src/Module/Event.php
+++ src/Module/Event.php
@@ -95,9 +95,6 @@ class Event {
      * @throws Exception
      */
     public static function trigger(App $object, $action, $options=[]){
-        $debug = debug_backtrace(true);
-        d($debug[0]['file'] . ':' . $debug[0]['line']);
-        d($options);
         $events = $object->get(App::EVENT)->select('event', [
             'action' => $action
         ]);
