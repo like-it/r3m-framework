3af894385cda0ea13bd4c83cd75560215d5f519a
remco van der Velde <remco.pc@outlook.com>
2023-05-23 20:07:07 +0200

    data restore

---
+++ src/Validate/Is.Date.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-09-18
+ * @copyright       Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *     -            all
+ */
+use R3m\Io\App;
+
+function validate_is_date(App $object, $string='', $field='', $argument=''): bool
+{
+    d($string);
+    d($field);
+    ddd($argument);
+    return false;
+}
