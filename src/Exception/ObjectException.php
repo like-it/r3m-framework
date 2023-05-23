f1d3157bec22290b0b289fa425887efed0877388
remco van der Velde <remco.pc@outlook.com>
2023-05-23 14:25:19 +0200

    data restore

--- src/Exception/ObjectException.php
+++ src/Exception/ObjectException.php
@@ -14,4 +14,14 @@ use Exception;
 
 class ObjectException extends Exception {
 
+
+    public function __toString()
+    {
+        $string = parent::__toString();
+        $string .= PHP_EOL;
+        $string .= $this->getMessage();
+        $string .= PHP_EOL;
+        return $string;
+    }
+
 }
