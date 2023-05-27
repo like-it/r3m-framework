23da403e8a82be07d6ce04871fceadf030cd1888
remco van der Velde <remco.pc@outlook.com>
2023-05-27 18:18:42 +0200

    middleware

--- src/App.php
+++ src/App.php
@@ -298,10 +298,17 @@ class App extends Data {
                         $functions[] = 'before_run';
                         $route->controller::before_run($object);
                     }
+                    $start = microtime(true);
                     $route = Middleware::trigger($object, [
                         'route' => $route,
                         'methods' => $methods,
                     ]);
+                    $duration = microtime(true) - $start;
+                    if($duration < 1) {
+                        echo 'Duration: ' . round($duration * 1000, 2) . ' msec url: ' . $options['url'] . PHP_EOL;
+                    } else {
+                        echo 'Duration: ' . round($duration, 2) . ' sec url:' . $options['url'] . PHP_EOL;
+                    }
                     if (in_array($route->function, $methods, true)) {
                         $functions[] = $route->function;
                         $object->config('controller.function', $route->function);
@@ -321,11 +328,18 @@ class App extends Data {
                             'route' => $route,
                             'response' => $result
                         ]);
+                        $start = microtime(true);
                         $result = OutputFilter::trigger($object, [
                             'route' => $route,
                             'methods' => $methods,
                             'response' => $result
                         ]);
+                        $duration = microtime(true) - $start;
+                        if($duration < 1) {
+                            echo 'Duration: ' . round($duration * 1000, 2) . ' msec url: ' . $options['url'] . PHP_EOL;
+                        } else {
+                            echo 'Duration: ' . round($duration, 2) . ' sec url:' . $options['url'] . PHP_EOL;
+                        }
                     } else {
                         $object->logger(App::LOGGER_NAME)->error(
                             'Controller (' .
