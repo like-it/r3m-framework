e02a491bdc190fbc93d04a389e5f1f48517b4712
remco van der Velde <remco.pc@outlook.com>
2023-05-28 22:06:03 +0200

    middleware

--- src/Module/Event.php
+++ src/Module/Event.php
@@ -203,6 +203,7 @@ class Event extends Main {
                     'ramdisk' => true
                 ]
             );
+            ddd($response);
             if(
                 $response &&
                 array_key_exists('list', $response)
