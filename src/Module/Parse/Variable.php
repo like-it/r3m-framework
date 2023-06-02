3433c5988e4e34f254fac37aaaa20782ae5bd11f
remco van der Velde <remco.pc@outlook.com>
2023-06-02 17:25:48 +0200

    middleware

--- src/Module/Parse/Variable.php
+++ src/Module/Parse/Variable.php
@@ -67,6 +67,8 @@ class Variable {
                             $add_quote = false;
                             $quote_add = false;
                             $attribute = '\'' . $variable['variable']['attribute'];
+                            d($attribute);
+                            d($execute);
                             foreach($execute as $part_nr => $part_record){
                                 if(substr($part_record, 0, 1) === '$'){
                                     if($part_nr === 0){
