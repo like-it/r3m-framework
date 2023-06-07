b744c03ebde5b758c6f0b6ef64d7f6f5445ddaa9
remco van der Velde <remco.pc@outlook.com>
2023-06-07 22:32:57 +0200

    middleware

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -447,7 +447,7 @@ class Parse {
                         '[$ldelim-' . $uuid . ']',
                         '[$rdelim-' . $uuid . ']',
                     ],
-                    "/**/\n" .  //row mismatch (+1)
+                    "/****/\n" .  //row mismatch (+1)
                     $explode[1]
                 );
                 $string = str_replace(
