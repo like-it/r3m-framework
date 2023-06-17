7c9760660733afeb4fe9924dc3c54711a97af3b5
remco van der Velde <remco.pc@outlook.com>
2023-06-17 13:10:42 +0200

    object_get

--- src/Module/Data.php
+++ src/Module/Data.php
@@ -287,7 +287,6 @@ class Data {
                 }
             }
             elseif($attribute == 'get'){
-                echo $value . PHP_EOL;
                 return Core::object_get($value, $this->data());
             }
             elseif($attribute == 'has'){
@@ -321,7 +320,6 @@ class Data {
                     $attribute = (string) $attribute;
                 }
                 if(is_string($attribute)){
-                    echo $attribute . PHP_EOL;
                     return Core::object_get($attribute, $this->data());
                 }
                 elseif(is_object($attribute) && get_class($attribute) === Data::class){
