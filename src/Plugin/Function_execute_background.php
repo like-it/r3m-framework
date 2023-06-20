a5f44fbc3a94eb2aa05df05449b14bf74028f802
remco van der Velde <remco.pc@outlook.com>
2023-06-20 20:30:40 +0200

    debug background task

--- src/Plugin/Function_execute_background.php
+++ src/Plugin/Function_execute_background.php
@@ -10,6 +10,7 @@
  */
 use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
+use R3m\Io\Module\Core;
 
 /**
  * @throws \R3m\Io\Exception\ObjectException
@@ -18,9 +19,10 @@ function function_execute_background(Parse $parse, Data $data, $command=''){
     $command = (string) $command;
     $command = escapeshellcmd($command);
     if(substr($command, 0, -1) !== '&'){
-        $command .= '&';
+//        $command .= '&';
     }
     d($command);
-    exec($command, $output);
+    Core::execute($parse->object(), $command, $output, $notification, Core::SHELL_PROCESS);
+//    exec($command, $output);
     return $output;
 }
