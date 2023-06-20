c301436e4c450fe0a90ff928b9e62a86b027b6c3
remco van der Velde <remco.pc@outlook.com>
2023-06-20 18:53:55 +0200

    function execute / execute_background

---
+++ src/Plugin/Function_execute_background.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-09-14
+ * @copyright       Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *     -            all
+ */
+use R3m\Io\Module\Parse;
+use R3m\Io\Module\Data;
+use R3m\Io\Module\Core;
+
+/**
+ * @throws \R3m\Io\Exception\ObjectException
+ */
+function function_execute_background(Parse $parse, Data $data, $command=''){
+    $command = (string) $command;
+    $command = escapeshellcmd($command);
+    if(substr($command, 0, -1) !== '&'){
+        $command .= ' &';
+    }
+    exec($command, $output);
+    return $output;
+}
