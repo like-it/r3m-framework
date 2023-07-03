d2dddfdc8e4c9fdffe5e6da0751cec3be51d5695
remco van der Velde <remco.pc@outlook.com>
2023-07-04 00:07:09 +0200

    debug data

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -817,6 +817,8 @@ class Core
             return null;
         }
         $keys = [];
+        var_dump($attributeList);
+        die;
         foreach($attributeList as $key_attribute => $value_attribute) {
             $is_collect[] = $key_attribute;
             $key_collect = implode('.', $is_collect);
