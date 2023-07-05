c856f3c5a342bbe978f58a3cb920ecf6fee5e49d
remco van der Velde <remco.pc@outlook.com>
2023-07-05 13:49:17 +0200

    r3m-io/node list

--- src/Config.php
+++ src/Config.php
@@ -234,6 +234,9 @@ class Config extends Data {
             array_key_exists(Config::DATA_DIR_VENDOR, $config)
         ){
             $this->data(Config::DATA_PROJECT_DIR_VENDOR, $config[Config::DATA_DIR_VENDOR]);
+            echo '2 ';
+            var_dump($this->data(Config::DATA_PROJECT_DIR_VENDOR));
+            die;
             $this->data(Config::DATA_PROJECT_DIR_ROOT, dirname($this->data(Config::DATA_PROJECT_DIR_VENDOR)) . '/');
             unset($config[Config::DATA_DIR_VENDOR]);
         }
