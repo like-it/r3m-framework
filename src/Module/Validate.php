942396332d3d114270273a38beb76bdacdc8c01f
remco van der Velde <remco.pc@outlook.com>
2023-07-06 19:01:58 +0200

    r3m-io/node list

--- src/Module/Validate.php
+++ src/Module/Validate.php
@@ -66,6 +66,7 @@ class Validate {
             } 
             elseif(is_array($list)){
                 $field_request = str_replace('[]', '', $field);
+                d($field_request);
                 d($object->request('node.' . $field_request));
                 if($object->request('has', 'node.' . $field_request)){
                     $value = $object->request('node.' . $field_request);
