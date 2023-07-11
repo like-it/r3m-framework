deb2859eee5cd20f0b50ac587087e5be98837296
remco van der Velde <remco.pc@outlook.com>
2023-07-11 12:10:15 +0200

    r3m-io/node list

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -620,9 +620,7 @@ class Controller {
             }
         }
         $original = $read;
-        d($original);
         $read = $parse->compile($read, $data, $parse->storage());
-        d($read);
         Parse::readback($object, $parse, App::SCRIPT);
         Parse::readback($object, $parse, App::LINK);
         return $read;
