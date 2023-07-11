f9e3bfefa7a2de91451ade33d201876442161473
remco van der Velde <remco.pc@outlook.com>
2023-07-11 15:09:24 +0200

    r3m-io/node list

--- src/Module/Template/Main.php
+++ src/Module/Template/Main.php
@@ -161,7 +161,9 @@ class Main {
         }
 	}
 
-	protected function value_minus($variable1=null, $variable2=null){		
+	protected function value_minus($variable1=null, $variable2=null){
+        d($variable1);
+        d($variable2);
 		$variable1 += 0;
 		$variable2 += 0;
 		return $variable1 - $variable2;        
