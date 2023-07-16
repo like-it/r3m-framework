112477d5f206f1351beac22cad7fd477ecdaa388
remco van der Velde <remco.pc@outlook.com>
2023-07-16 22:25:36 +0200

    added cache module

--- src/Module/Cache.php
+++ src/Module/Cache.php
@@ -21,6 +21,95 @@ use R3m\Io\Exception\ObjectException;
 
 class Cache {
 
+    const ROUTE = 'route';
+    const REQUEST = 'request';
+    const SESSION = 'session';
+    const OBJECT = 'object';
+    const TTL = 'ttl';
+    const URL = 'url';
+
+    const ONE_MINUTE = 60;
+    const TWO_MINUTES = 120;
+    const THREE_MINUTES = 180;
+    const FOUR_MINUTES = 240;
+    const FIVE_MINUTES = 300;
+    const SIX_MINUTES = 360;
+    const SEVEN_MINUTES = 420;
+    const EIGHT_MINUTES = 480;
+    const NINE_MINUTES = 540;
+    const TEN_MINUTES = 600;
+    const FIFTEEN_MINUTES = 900;
+    const TWENTY_MINUTES = 1200;
+    const TWENTY_FIVE_MINUTES = 1500;
+    const THIRTY_MINUTES = 1800;
+    const THIRTY_FIVE_MINUTES = 1800;
+    const FORTY_MINUTES = 2400;
+    const FORTY_FIVE_MINUTES = 2700;
+    const FIFTY_MINUTES = 3000;
+    const FIFTY_FIVE_MINUTES = 3300;
+    const SIXTY_MINUTES = 3000;
+
+    const ONE_HOUR = 3600;
+    const TWO_HOURS = 7200;
+    const THREE_HOURS = 10800;
+    const FOUR_HOURS = 14400;
+    const FIVE_HOURS = 18000;
+    const SIX_HOURS = 21600;
+    const SEVEN_HOURS = 25200;
+    const EIGHT_HOURS = 28800;
+    const NINE_HOURS = 32400;
+    const TEN_HOURS = 36000;
+    const ELEVEN_HOURS = 39600;
+    const TWELVE_HOURS = 43200;
+    const THIRTEEN_HOURS = 46800;
+    const FOURTEEN_HOURS = 50400;
+    const FIFTEEN_HOURS = 54000;
+    const SIXTEEN_HOURS = 57600;
+    const SEVENTEEN_HOURS = 61200;
+    const EIGHTEEN_HOURS = 64800;
+    const NINETEEN_HOURS = 68400;
+    const TWENTY_HOURS = 72000;
+    const TWENTY_ONE_HOURS = 75600;
+    const TWENTY_TWO_HOURS = 79200;
+    const TWENTY_THREE_HOURS = 82800;
+    const TWENTY_FOUR_HOURS = 86400;
+
+    const ONE_DAY = 86400;
+    const TWO_DAYS = 172800;
+    const THREE_DAYS = 259200;
+    const FOUR_DAYS = 345600;
+    const FIVE_DAYS = 432000;
+    const SIX_DAYS = 518400;
+    const SEVEN_DAYS = 604800;
+    const ONE_WEEK = 604800;
+    const TWO_WEEKS = 1209600;
+    const THREE_WEEKS = 1814400;
+    const FOUR_WEEKS = 2419200;
+    const ONE_MONTH = 2592000;
+    const TWO_MONTHS = 5184000;
+    const THREE_MONTHS = 7776000;
+    const ONE_QUARTER = 7776000;
+    const TWO_QUARTERS = 15552000;
+    const THREE_QUARTERS = 23328000;
+    const FOUR_QUARTERS = 31536000;
+    const ONE_YEAR = 31536000;
+    const TWO_YEARS = 63072000;
+    const THREE_YEARS = 94608000;
+    const FOUR_YEARS = 126144000;
+    const FIVE_YEARS = 157680000;
+
+    const INF = 'INF';
+
+    public static function url($object, $type=Cache::REQUEST){
+        switch($type){
+            case Cache::ROUTE:
+                $class_call = constant(get_called_class() . '::class');
+                ddd($class_call);
+                $url = $object->route()->current();
+        }
+
+    }
+
     /**
      * @throws ObjectException
      * @throws Exception
