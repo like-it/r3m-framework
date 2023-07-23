a5f8c98859e8ae52a9d4a2ff9595ff124632d8f1
remco van der Velde <remco.pc@outlook.com>
2023-07-23 21:40:22 +0200

    shared memory

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -203,6 +203,7 @@ class Controller {
                 ],
                 $name
             );
+            ddd($object->config('controller.dir.view'));
             if(!empty($object->config('controller.dir.view'))){
                 $list[] = $object->config('controller.dir.view') .
                     str_replace('.', $object->config('ds'), $name) .
