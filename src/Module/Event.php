f6338840bc20b1d42265ea8a245d34966e23b5e4
remco van der Velde <remco.pc@outlook.com>
2023-08-18 23:52:38 +0200

    patch_nested_key (first one way only)

--- src/Module/Event.php
+++ src/Module/Event.php
@@ -211,6 +211,7 @@ class Event extends Main {
                     'ramdisk' => true
                 ]
             );
+            d($response);
             if(
                 $response &&
                 array_key_exists('list', $response)
